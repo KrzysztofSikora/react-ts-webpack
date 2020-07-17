7f2597a5a23704583163c2e6503bc7e1961df087
KrzysztofSikora <listdlakrzycha@gmail.com>
2020-07-17 10:25:24 +0200

    Hooks and redux

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import { Store } from './stores/Store';
+import { Link } from '@reach/router';
+
+export default function App(props: any): JSX.Element {
+    const { state } = React.useContext(Store)
+    return (
+        <React.Fragment>
+            <header className="header">
+                <div>
+                    <h1>title</h1>
+                    <p>Pick your favourite episode!!!</p>
+                </div>
+                <div>
+                    <Link to='/'>Home</Link>
+                    <Link to='/faves'>Favourite(s): {state.favourites.length}</Link>
+                </div>
+            </header>
+            {props.children}
+        </React.Fragment>
+    )
+}
